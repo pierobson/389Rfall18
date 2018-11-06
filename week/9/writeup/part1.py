78db1e5e6bf5919e5a1ff3c08660c5a53660031a
Pierce Robson <piercerobson@gmail.com>
2018-11-05 20:26:27 -0500

    Part 1 script done

---
+++ week/9/writeup/part1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+# importing a useful library -- feel free to add any others you find necessary
+import hashlib
+import string
+
+# this will work if you place this script in your writeup folder
+pswds = open("passwords.txt", 'a+')
+
+# a string equal to 'abcdefghijklmnopqrstuvwxyz'.
+salts = string.ascii_lowercase
+count = 1
+for salt in salts:
+    print('----- '+ salt + ' -----')
+    wordlist = open("pwd.txt", 'r')
+    for word in wordlist:
+        count += 1
+        h = hashlib.sha512()
+        pw = salt + word.strip()
+#        print(pw)
+        h.update(pw)
+        hsh = h.hexdigest()
+#        print(hsh)
+        hashes = open("../hashes", 'r')
+        for hashpass in hashes:
+            if hsh == hashpass.strip():
+                pswds.write(pw + " == " + hashpass + "\n\n")
+                        
+print(count)
